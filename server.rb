67ab66dc4b05bb88f00fc23f3048ea7252be53fa
lizconlan <lizconlan@gmail.com>
2012-10-28 03:25:46 +0000

    pagination; better url handling

--- server.rb
+++ server.rb
@@ -19,23 +19,28 @@ before do
 end
 
 post "/" do
-  do_search
+  query = params[:query]
+  redirect "/#{query}"
   haml(:"search")
 end
 
-get "/" do
+get "/:query" do
   do_search
   haml(:"search")
 end
 
 def do_search
+  page = params[:page]
+  @page = page ? page.to_i : 1
+  @page = 1 if @page < 1
+  @results_start = (@page-1)*10+1
+  
   query = params[:query]
   if query
     #reference = HansardReference.create_from(query)
     
-    
     @page_title = "Search: #{query}"
-    url = WEBSOLR_URL + "/select/?q=solr_text_texts:#{CGI::escape(query)}&facet=true&facet.field=decade_is&facet.field=year_is&facet.field=sitting_type_ss&facet.field=speaker_uid_ss&wt=json&hl.fragsize=150&hl=true&hl.fl=solr_text_texts&facet.zeros=false"
+    url = WEBSOLR_URL + "/select/?q=solr_text_texts:#{CGI::escape(query)}&start=#{@results_start-1}&facet=true&facet.field=decade_is&facet.field=year_is&facet.field=sitting_type_ss&facet.field=speaker_uid_ss&wt=json&hl.fragsize=150&hl=true&hl.fl=solr_text_texts&facet.zeros=false"
     #&sort=date_ds+desc
     #&fq=speaker_name_ss:%22Mr%20Isaac%20Corry%22
     #&facet.query=decade_is:1800
@@ -43,16 +48,18 @@ def do_search
     response = RestClient.get(url)
     result = JSON.parse(response)
   
-    html = []
+    @search_results = []
   
     speaker_data = result["facet_counts"]["facet_fields"]["speaker_uid_ss"]
     if speaker_data.is_a?(Array)
       @speaker_facets = facets_to_hash(speaker_data)
     end
   
-    @found = result["response"]["numFound"]
-    @search_results = []
-  
+    @results_found = result["response"]["numFound"]
+    @results_end = @results_start + 9
+    @results_end = @results_found if @results_end > @results_found
+    @last_page = (@results_found / 10.0).ceil
+    
     result["response"]["docs"].each do |search_result|
       id = search_result["id"]
       @search_results << SearchResult.new(search_result["subject_ss"], search_result["url_ss"], search_result["speaker_uid_ss"], search_result["sitting_type_ss"], search_result["date_ds"], result["highlighting"][id]["solr_text_texts"].join(" "))
