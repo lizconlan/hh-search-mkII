44891a163b5175612a9b35e68b8b1ae56d0a82b7
lizconlan <lizconlan@gmail.com>
2012-10-29 10:03:11 +0000

    changing fragsize to 200

--- server.rb
+++ server.rb
@@ -43,7 +43,7 @@ def do_search
     #reference = HansardReference.create_from(query)
     
     @page_title = "Search: #{query}"
-    url = WEBSOLR_URL + "/select/?q=text_texts:#{CGI::escape(query)}&start=#{@results_start-1}&facet=true&facet.field=decade_is&facet.field=year_is&facet.field=sitting_type_ss&facet.field=speaker_uid_ss&wt=json&hl.fragsize=150&hl=true&hl.fl=text_texts&facet.zeros=false"
+    url = WEBSOLR_URL + "/select/?q=text_texts:#{CGI::escape(query)}&start=#{@results_start-1}&facet=true&facet.field=decade_is&facet.field=year_is&facet.field=sitting_type_ss&facet.field=speaker_uid_ss&wt=json&hl.fragsize=200&hl=true&hl.fl=text_texts&facet.zeros=false"
     #&sort=date_ds+desc
     #&fq=speaker_name_ss:%22Mr%20Isaac%20Corry%22
     #&facet.query=decade_is:1800
