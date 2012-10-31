e2a5eb8e4465a0346d6e666bc5796eaae18d7bff
lizconlan <lizconlan@gmail.com>
2012-10-31 20:53:44 +0000

    cleanup in aisle 3

--- lib/search.rb
+++ lib/search.rb
@@ -27,10 +27,8 @@ class Search
     response = RestClient.get(url)
     result = JSON.parse(response)
     
-    #raise result.inspect
-
     @search_results = []
-
+    
     result["response"]["docs"].each do |search_result|
       id = search_result["id"]
       @search_results << SearchResult.new(search_result["subject_ss"], search_result["url_ss"], search_result["speaker_uid_ss"], search_result["sitting_type_ss"], search_result["date_ds"], result["highlighting"][id]["text_texts"].join(" "))
@@ -46,16 +44,4 @@ class Search
     @results_end = @results_found if @results_end > @results_found
     @last_page = (@results_found / 10.0).ceil
   end
-  
-  
-  # def self.url_builder(new_option)
-  #   self.sort = options[:sort]
-  #   self.page = options[:page] || 1
-  #   self.num_per_page = options[:num_per_page] || 10
-  #   self.speakers_to_display = options[:speakers_to_display] || 5
-  #   self.query = clean_query(options[:query])
-  #   self.sitting_type = options[:type]
-  #   self.speaker = Person.find_by_slug(options[:speaker]) if options[:speaker]
-  #   self.display_all_speakers = options[:all_speaker_filters]
-  # end
 end
