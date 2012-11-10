c01c7707767704eea203a631509505adf559c723
lizconlan <lizconlan@gmail.com>
2012-11-10 12:33:10 +0000

    removed unused code from Person

--- models/person.rb
+++ models/person.rb
@@ -1,11 +1,2 @@
 class Person < ActiveRecord::Base
-  
-  def name
-    if / of$/.match(honorific)
-      part_list = [honorific, lastname]
-    else
-      part_list = [honorific, firstname, lastname]
-    end
-    part_list.join(' ').strip
-  end
 end
