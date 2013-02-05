69411308c6001c64007ef53eb0742b0ddf09f442
lizconlan <lizconlan@gmail.com>
2013-02-05 23:39:40 +0000

    a bit of explanation for the monkeypatch

--- lib/active_record_monkeypatch.rb
+++ lib/active_record_monkeypatch.rb
@@ -1,3 +1,5 @@
+# stuffing the old subclasses class method inelegantly back into ActiveRecord
+# but purely for the sake of running acts_as_solr
 class ActiveRecord::Base
   def self.subclasses
     self.descendants
