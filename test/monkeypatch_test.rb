6184b6bb782a8a240c0340322df64222d4c8cc3e
lizconlan <lizconlan@gmail.com>
2013-02-05 23:49:00 +0000

    better monkeypatch tests

--- test/monkeypatch_test.rb
+++ test/monkeypatch_test.rb
@@ -4,12 +4,23 @@ require_relative '../models/contribution'
 
 class MonkeypatchTest < MiniTest::Spec
   describe "MonkeyPatch" do
-    it "should allow ActiveRecord to respond to 'subclasses'" do
+    it "should allow ActiveRecord::Base to respond to 'subclasses'" do
       ActiveRecord::Base.must_respond_to(:subclasses)
     end
     
     it "should allow objects inheriting from ActiveRecord::Base to respond to 'subclasses'" do
       Contribution.must_respond_to(:subclasses)
+    end
+    
+    describe "when calling self.subclass on ActiveRecord::Base" do
+      it "should call the class method descendants" do
+        ActiveRecord::Base.expects(:descendants)
+        ActiveRecord::Base.subclasses
+      end
+    
+      it "should return an array of classes that inherit from the class" do
+        ActiveRecord::Base.subclasses.must_equal([Contribution, Section, Person])
+      end
     end
   end
 end
