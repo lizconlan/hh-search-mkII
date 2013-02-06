d44059f30d1020480352dcc5616d0ce76421e4e3
lizconlan <lizconlan@gmail.com>
2013-02-06 12:10:17 +0000

    tweaking the testing/coverage setup

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,6 +2,9 @@ if ENV['COVERAGE']
   require 'simplecov'
   SimpleCov.start do
     add_filter 'test'
+    add_group "Models", "models"
+    add_group "Libraries", "lib"
+    add_group "Helpers", "helpers"
   end
 end
 
