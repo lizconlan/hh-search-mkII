851e256651ade55c2e5bc71ea46d16d610e22998
lizconlan <lizconlan@gmail.com>
2013-02-06 13:50:07 +0000

    basic tests for the Contribution model

---
+++ test/models/contribution_test.rb
@@ -0,0 +1,238 @@
+require_relative '../minitest_helper.rb'
+require_relative '../../models/contribution.rb'
+
+class ContributionTest < MiniTest::Spec
+  describe Contribution do
+    describe "in general" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "HouseOfCommonsSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor",
+         })
+         
+      it "should set the shared attributes correctly" do
+        test_contribution.year.must_equal(1805)
+        test_contribution.century.must_equal(19)
+        test_contribution.decade.must_equal(1800)
+      end
+      
+      it "should return nil for all speaker-related methods if there is no speaker" do
+        test_contribution.speaker_slug.must_equal(nil)
+        test_contribution.speaker_name.must_equal(nil)
+      end
+      
+      it "should return nil for start and end columns if no column data is stored" do
+        test_contribution.start_column.must_equal(nil)
+        test_contribution.end_column.must_equal(nil)
+      end
+      
+      it "should return an empty array for cols if no column data is stored" do
+        test_contribution.cols.must_equal([])
+      end
+    end
+    
+    describe "when given a contribution with just a start column" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "HouseOfCommonsSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor",
+         :column_range => "42"
+         })
+      
+      it "should return a single element array for cols" do
+        test_contribution.cols.must_equal(["42"])
+      end
+      
+      it "should return identical start and end columns" do
+        test_contribution.start_column.must_equal("42")
+        test_contribution.end_column.must_equal("42")
+      end
+    end
+    
+    describe "when given a contribution with start column and end columns" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "HouseOfCommonsSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor",
+         :column_range => "42,44"
+         })
+      
+      it "should return a 2 element array for cols" do
+        test_contribution.cols.must_equal(["42","44"])
+      end
+      
+      it "should return the start and end columns" do
+        test_contribution.start_column.must_equal("42")
+        test_contribution.end_column.must_equal("44")
+      end
+    end
+    
+    describe "when given a contribution with a speaker" do
+      person = Person.new(
+        {:honorific => "Mr",
+         :name => "Jim Hacker",
+         :slug => "mr-jim-hacker"
+        })
+      
+      test_contribution = Contribution.new(
+        {:sitting_type => "HouseOfCommonsSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor",
+         :person => person
+         })
+      
+      it "should correctly respond to the speaker-related methods" do
+        test_contribution.speaker_slug.must_equal("mr-jim-hacker")
+        test_contribution.speaker_name.must_equal("Mr Jim Hacker")
+      end
+    end
+    
+    describe "when given a House of Commons Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "HouseOfCommonsSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+      
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Commons")
+        test_contribution.section_link.must_equal("commons")
+        test_contribution.house.must_equal("Commons")
+        test_contribution.url.must_equal("commons/1805/nov/10/test#anchor")
+      end
+    end
+    
+    describe "when given a Westminster Hall Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "WestminsterHallSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+      
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Westminster Hall")
+        test_contribution.section_link.must_equal("westminster_hall")
+        test_contribution.house.must_equal("Commons")
+        test_contribution.url.must_equal("westminster_hall/1805/nov/10/test#anchor")
+      end
+    end
+    
+    describe "when given a Commons Written Answers Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "CommonsWrittenAnswersSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+      
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Written Answers")
+        test_contribution.section_link.must_equal("written_answers")
+        test_contribution.house.must_equal("Commons")
+        test_contribution.url.must_equal("written_answers/1805/nov/10/test#anchor")
+      end
+    end
+    
+    describe "when given a Commons Written Statements Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "CommonsWrittenStatementsSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+      
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Written Statements")
+        test_contribution.section_link.must_equal("written_statements")
+        test_contribution.house.must_equal("Commons")
+        test_contribution.url.must_equal("written_statements/1805/nov/10/test#anchor")
+      end
+    end
+    
+    describe "when given a House of Lords Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "HouseOfLordsSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+         
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Lords")
+        test_contribution.section_link.must_equal("lords")
+        test_contribution.house.must_equal("Lords")
+        test_contribution.url.must_equal("lords/1805/nov/10/test#anchor")
+      end
+    end
+    
+    describe "when given a Grand Committee Report Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "GrandCommitteeReportSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+         
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Grand Committee report")
+        test_contribution.section_link.must_equal("grand_committee_report")
+        test_contribution.house.must_equal("Lords")
+        test_contribution.url.must_equal("grand_committee_report/1805/nov/10/test#anchor")
+      end
+    end
+    
+    describe "when given a Lords Report Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "HouseofLordsReport",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+         
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Lords Reports")
+        test_contribution.section_link.must_equal("lords_reports")
+        test_contribution.house.must_equal("Lords")
+        test_contribution.url.must_equal("lords_reports/1805/nov/10/test#anchor")
+      end
+    end
+    
+    describe "when given a Lords Written Answers Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "LordsWrittenAnswersSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+      
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Written Answers")
+        test_contribution.section_link.must_equal("written_answers")
+        test_contribution.house.must_equal("Lords")
+        test_contribution.url.must_equal("written_answers/1805/nov/10/test#anchor")
+      end
+    end
+    
+    describe "when given a Lords Written Statements Sitting" do
+      test_contribution = Contribution.new(
+        {:sitting_type => "LordsWrittenStatementsSitting",
+         :date => Date.parse("1805-11-10"),
+         :slug => "test",
+         :anchor_id => "anchor"
+         })
+      
+      it "should set the sitting-specific attributes correctly" do
+        test_contribution.display_sitting_type.must_equal("Written Statements")
+        test_contribution.section_link.must_equal("written_statements")
+        test_contribution.house.must_equal("Lords")
+        test_contribution.url.must_equal("written_statements/1805/nov/10/test#anchor")
+      end
+    end
+  end
+end
