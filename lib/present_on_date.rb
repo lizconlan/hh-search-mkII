6e9885182b5a494b3d01e1e642c1c978e3334da3
lizconlan <lizconlan@gmail.com>
2013-02-08 12:58:32 +0000

    removing the last of the unused code from present_on_date

--- lib/present_on_date.rb
+++ lib/present_on_date.rb
@@ -10,30 +10,6 @@ module PresentOnDate
   end
 
   module ClassMethods 
-    
-    # In the ActiveRecord model you wish to make available
-    # by one or more date fields, add a call to acts_as_present_on_date
-    # below the class definition.
-    #
-    # For example, if you want to expose :start_date and :end_date
-    #
-    #   acts_as_present_on_date [:start_date, :end_date]
-    #
-    # Other examples:
-    # 
-    #   acts_as_present_on_date [:start_date, :end_date],
-    #       :title => 'debate_title',
-    #       :url_method => 'individual_path',
-    #       :url_parameter => 'individual'
-    #
-    #   acts_as_present_on_date :date,
-    #       :title => 'individual.fullname',
-    #       :url_helper_method => 'individual_path',
-    #       :url_parameter => 'individual'
-    #
-    # This will automatically make all the methods in this module available
-    # in the model.
-    #
     def acts_as_present_on_date(fields, options={})
       cattr_accessor :present_on_date_fields
       cattr_accessor :present_on_date_options
@@ -43,9 +19,6 @@ module PresentOnDate
 
       fields.each do |field|
         raise 'field must be a symbol: ' + field.to_s unless field.is_a?(Symbol)
-      end
-      options.each_value do |option|
-        raise 'option value must be a string: ' + option.to_s unless option.is_a?(String)
       end
       
       self.present_on_date_fields = fields
@@ -65,7 +38,7 @@ module PresentOnDate
             :select => date_field,
             :conditions => date_condition)
         
-        dates += found_in_interval.collect { |m| m.send(date_field) }
+        dates += found_in_interval.map { |m| m.send(date_field) }
       end
       dates
     end
